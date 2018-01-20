c3b58c5b96c6fabf171eb23e3c9dd67e7e4565a6
wavejoker <lmxzct@163.com>
2018-01-20 13:34:20 +0800

    2018第一次更新

--- welife-sso/src/main/java/com/welife/sso/service/UseService.java
+++ welife-sso/src/main/java/com/welife/sso/service/UseService.java
@@ -9,40 +9,39 @@ import com.welife.pojo.WeUser;
 
 /**
  * 用户登录Service
- * 
- * @TODO 实现登录验证码功能
- * @author Creeper
- * @Data 2017年7月29日 下午1:49:14
  *
+ * @author Creeper
+ * @TODO 实现登录验证码功能
+ * @Data 2017年7月29日 下午1:49:14
  */
 public interface UseService {
 
-	/**
-	 * 检测用户注册信息是否可用
-	 * 
-	 */
-	public WeLifeResult checkData(String content, Integer type);
+    /**
+     * 检测用户注册信息是否可用
+     */
+    WeLifeResult checkData(String content, Integer type);
 
-	/**
-	 * 用户注册Service
-	 */
-	public WeLifeResult creatUser(WeUser user);
+    /**
+     * 用户注册Service
+     */
+    WeLifeResult creatUser(WeUser user);
 
-	/**
-	 * 用户登录Service
-	 * @param response 
-	 * @param request 
-	 */
-	public WeLifeResult userLogin(String username, String password, HttpServletRequest request,
-                                  HttpServletResponse response);
+    /**
+     * 用户登录Service
+     *
+     * @param response
+     * @param request
+     */
+    WeLifeResult userLogin(String username, String password, HttpServletRequest request,
+                           HttpServletResponse response);
 
-	/**
-	 * 通过token查询用户信息
-	 */
-	public WeLifeResult getUserByToken(String token);
+    /**
+     * 通过token查询用户信息
+     */
+    WeLifeResult getUserByToken(String token);
 
-	/**
-	 * 用户安全退出的Service
-	 */
-	public WeLifeResult userLogout(String token);
+    /**
+     * 用户安全退出的Service
+     */
+    WeLifeResult userLogout(String token);
 }
